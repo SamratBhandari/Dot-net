103628b8c7f2590adcfb3ff9649f0217a78d5e2b
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-02 17:24:55 +0545

    Generics Example

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿bool equal = Calculator.AreEqual(4,4);
+Console.WriteLine(equal);
+//bool equal = Calculator.AreEqual(“Nepal”, “Switzerland”);
+
+public class Calculator
+	{
+		public static bool AreEqual(int value1, int value2)
+			{
+				return value1.Equals(value2);
+			}
+	}
